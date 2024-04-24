98e22a2777381131c01e4aa76342a19ecc49c664
Ricard Hw <ric4rd11@gmail.com>
2024-04-23 20:29:48 -0600

    Finished AddRound module with test bench

--- Modules/AddRound/AddRound.vhd
+++ Modules/AddRound/AddRound.vhd
@@ -11,44 +11,98 @@
 -- Additional Comments: 
 --
 ----------------------------------------------------------------------------------
-
--- Commonly used libraries
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
-use IEEE.std_logic_unsigned.all;
 
--- Entity declaration
-entity YourEntityName is
+entity AddRoundKey_dp is
     Port (
         -- Input ports
-        input_port_1 : in std_logic;
-        input_port_2 : in std_logic;
+        DataIn : in std_logic_vector(127 downto 0);
+        DataKey : in std_logic_vector(127 downto 0);
+		  clk : in std_logic;
+		  start : in std_logic;
+		  reset : in std_logic;
         -- Output ports
-        output_port_1 : out std_logic;
-        output_port_2 : out std_logic
-    );
-end YourEntityName;
+        DataOut: out std_logic_vector(127 downto 0);
+		  finish : out std_logic);
+end AddRoundKey_dp;
 
--- Architecture definition
-architecture Behavioral of YourEntityName is
-
+architecture Behavioral of AddRoundKey_dp is
     -- Internal signals
-    signal internal_signal : std_logic;
+	 -- Temporary Variable used for the transformation
+    shared variable InternalData : std_logic_vector(127 downto 0);
+	 
+	 -- FSM signals
+  type state_values is (St0,St1,St2);
+  signal next_state, present_state : state_values;
 
 begin
-    -- Concurrent statements
-    output_port_1 <= input_port_1 and input_port_2;
-    output_port_2 <= input_port_1 or input_port_2;
 
-    -- Sequential process
-    process (input_port_1, input_port_2)
-    begin
-        if input_port_1 = '1' then
-            internal_signal <= '1';
-        else
-            internal_signal <= '0';
-        end if;
-    end process;
+	-- Section 1: Communications with the Master FSM
+	-- State Register Process
+	-- Holds the current state of the FSM
+	statereg: process (clk, reset)
+	begin
+    -- Asynchronous Reset
+    if reset = '1' then
+       present_state <= St0;
+     elsif rising_edge(clk) then
+       present_state <= next_state;
+     end if;
+  end process statereg;
+  
+  -- Define the Next-State Logic Process
+  -- Will obtain the next state based on the inputs and current state
+  fsm: process (present_state, start)
+  begin
+    case present_state is
+      when St0 => 
+          if start = '0' then
+            next_state <= St0;
+          else
+            next_state <= St1; 
+          end if;
+      when St1 =>
+            next_state <= St2;
+      when St2 =>
+          if start = '0' then
+            next_state <= St0;
+          else
+            next_state <= St2; 
+          end if;
+      when others =>
+            next_state <= St0;
+     end case;
+  end process fsm;
+  
+  -- The output of a Moore Machine is determined by the present_state only
+  output: process (present_state)
+  begin
+    case present_state is
+       when St0 =>
+          finish <= '0';
+       when St1 =>
+          finish <= '0';
+       when St2 =>
+          finish <= '1';
+      when others =>
+          finish <= '0';
+    end case;
+  end process output;
+	
+	
+	-- Process for AddRoundKeys operation
 
-end Behavioral;
+	operation : process(clk, present_state)
+	begin
+		if (rising_edge(clk) and present_state = St1) then
+	 
+		-- XOR operation between DataIn and DataKey, store result in InternalData
+		InternalData := DataIn xor DataKey;
+		end if;
+		-- Sent transformation to the output.
+		DataOut <= InternalData;
+	end process operation;
+
+end architecture Behavioral;
